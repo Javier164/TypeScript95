5dfeee11feece78093adf7fd4289b4596f05b82b
Javier Zayas <javierz06@outlook.com>
2022-11-27 19:39:02 -0500

    fixed OEM key generation

--- main.ts
+++ main.ts
@@ -32,17 +32,16 @@ class Win95 {
 		let years = [1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003]; // All valid OEM years.
 
 		let year = String(years[Math.floor(Math.random() * years.length)]).substring(2);
-		let dividend: number | string | null = Math.floor((Math.random() * 9_999_999) + 1_000_000); // Underscored numbers for readability.
-		dividend = Number("0" + String(dividend));
+		let dividend: number | string | null = Math.floor((Math.random() * 99_999) + 1).toString().padStart(6, "0"); // Underscored numbers for readability.
 
 		let garbage: number = Math.floor((Math.random() * 99_999) + 10_000); // Underscored numbers for readability.
 
 		let numbers: number[] | null = String(dividend).split('').map(Number);
 		let sum: number | null; numbers.forEach(element => (sum! += element));
 
-		while (((sum! % 7) != 0 ) || (numbers[7] == 0 || numbers[7] >= 8) || dividend.toString().length > 7 || garbage.toString().length < 5) { // Checks if the variables are valid. 
+		while (((sum! % 7) != 0 ) || (numbers[7] == 0 || numbers[7] >= 8) || dividend.toString().length > 7 || garbage.toString().length > 5) { // Checks if the variables are valid. 
 			dividend = null, numbers = null, sum = null; // Setting variables to have null values, since they aren't valid.
-			dividend = Math.floor((Math.random() * 9_999_999) + 1_000_000);
+			dividend = Math.floor((Math.random() * 99_999) + 1).toString().padStart(6, "0");
 			garbage = Math.floor((Math.random() * 99_999) + 10_000);
 			numbers = String(dividend).split('').map(Number); // More number generation!
 			numbers.forEach(element => (sum! += element)); 
@@ -57,7 +56,10 @@ class Win95 {
 		}
 
 		for (let i = 0; i < 25; i++) {
-			console.log(`Windows 95 OEM Key ${i}: ${Windows.OEM()}`); // Prints a bunch of OEM keys to test.
+			console.log(`Windows 95 OEM Key ${i}: ${Windows.OEM()}`); // Prints a bunch of CD keys to test.
 		}
 	}
 }
+
+let Windows = new Win95();
+Windows.Test();
